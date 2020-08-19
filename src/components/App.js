abef4b7a32c2e3d9d3f19a3369c7da3028d64020
pbrayer <skellocrusha@yahoo.com>
2020-08-19 16:50:08 -0500

    testing

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignUpPage from "./SignUp"
+
+function App() {
+  return (
+    <div className="App">
+      <SignUpPage></SignUpPage>
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
